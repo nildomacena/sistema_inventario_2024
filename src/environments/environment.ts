c1cfa6ce971e6e9b2be2693acc24681d38fb6b0b
Ednildo Macena <nildo.777@gmail.com>
2024-11-18 12:05:55 -0300

    Remove Firebase configuration files and environment settings

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://nhqxpwsuigcfbroqisdj.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ocXhwd3N1aWdjZmJyb3Fpc2RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4MjY0NTAsImV4cCI6MjAzNzQwMjQ1MH0.ITiVGNb6vtfqOY7hiYY09bATrq32GI27-qoULU4tCyw',
-}
